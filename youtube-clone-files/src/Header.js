aa2c5902f4576aacb7c60815700bb3c2842c25e1
Juanse1595 <jspbescos15@gmail.com>
2022-08-28 18:14:43 -0500

    ADD Heades.css > with styles for the header, UPD Header.js > added 3 divs to contain the 3 main parts of the header

--- youtube-clone-files/src/Header.js
+++ youtube-clone-files/src/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import "./Header.css";
 import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
 import VideoCallIcon from '@mui/icons-material/VideoCall';
@@ -10,20 +11,28 @@ import { Avatar } from '@mui/material';
 function Header() {
   return (
     <div className='header'>
-      <MenuIcon />
-      <img
-        className='header__logo'
-        src="https://upload.wikimedia.org/wikipedia/commons/9/9e/YouTube_Logo_%282013-2017%29.svg"
-        alt='youtube-logo' />
-      <input type="text" />
-      <SearchIcon />
-      <VideoCallIcon />
-      <AppsIcon />
-      <NotificationsIcon />
-      <Avatar
-        alt="profile-button"
-        src="https://avatars.githubusercontent.com/u/85318393?s=400&u=b564d5fe1d357e061a59e5028348bcb5e570ab36&v=4"
-      />
+      <div className='header__left'>
+        <MenuIcon />
+        <img
+          className='header__logo'
+          src="https://upload.wikimedia.org/wikipedia/commons/9/9e/YouTube_Logo_%282013-2017%29.svg"
+          alt='youtube-logo' />
+      </div>
+
+      <div className='header__input'>
+        <input type="text" />
+        <SearchIcon />
+      </div>
+
+      <div className='header__icons'>
+        <VideoCallIcon />
+        <AppsIcon />
+        <NotificationsIcon />
+        <Avatar
+          alt="profile-button"
+          src="https://avatars.githubusercontent.com/u/85318393?s=400&u=b564d5fe1d357e061a59e5028348bcb5e570ab36&v=4"
+        />
+      </div>
     </div>
   )
 }
