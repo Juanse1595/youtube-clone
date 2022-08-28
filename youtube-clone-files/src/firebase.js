7f8cf7d60799edda35bac38fdbf47eac56a637f3
Juanse1595 <jspbescos15@gmail.com>
2022-08-27 21:31:39 -0500

    ADD youtube-clone-files directory with the react app created

---
+++ youtube-clone-files/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBC6EUKb2m1w0VGrXNLJgh6bAbgqQMZld0",
+  authDomain: "clone-2abc2.firebaseapp.com",
+  projectId: "clone-2abc2",
+  storageBucket: "clone-2abc2.appspot.com",
+  messagingSenderId: "529613947064",
+  appId: "1:529613947064:web:8192445e9824a4b5e9f44d",
+  measurementId: "G-SS503GQ1E5"
+};
