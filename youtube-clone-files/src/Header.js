fcae667fb226f0f93a2a186fdfb203ffa5a1387c
Juanse1595 <jspbescos15@gmail.com>
2022-08-28 16:33:58 -0500

    UPD package.json inst> alled mui icons-material for the icons, Header.js > added icons from mui library MenuIcon SearchIcon VideoCallIcon AppsIcon NotificationsIcon and Avatar

--- youtube-clone-files/src/Header.js
+++ youtube-clone-files/src/Header.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import VideoCallIcon from '@mui/icons-material/VideoCall';
+import AppsIcon from "@mui/icons-material/Apps";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import { Avatar } from '@mui/material';
+
 
 function Header() {
   return (
     <div className='header'>
-      <h1>I am a header</h1>
+      <MenuIcon />
+      <img
+        className='header__logo'
+        src="https://upload.wikimedia.org/wikipedia/commons/9/9e/YouTube_Logo_%282013-2017%29.svg"
+        alt='youtube-logo' />
+      <input type="text" />
+      <SearchIcon />
+      <VideoCallIcon />
+      <AppsIcon />
+      <NotificationsIcon />
+      <Avatar
+        alt="profile-button"
+        src="https://avatars.githubusercontent.com/u/85318393?s=400&u=b564d5fe1d357e061a59e5028348bcb5e570ab36&v=4"
+      />
     </div>
   )
 }
