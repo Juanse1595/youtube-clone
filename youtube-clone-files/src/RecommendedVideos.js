c3ef311a562e9da01a9a085037770dece45ee693
Juanse1595 <jspbescos15@gmail.com>
2022-08-31 17:55:07 -0500

    ADD RecommendedVideos.js and .css that contians the component for the videos section, Sidebar.js and .css contains the component of the left sidebar, UPD App.js added div for sidebar and recommended videos, App.css added display flex so both components are distributed horizontaly

---
+++ youtube-clone-files/src/RecommendedVideos.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './RecommendedVideos.css';
+
+function RecommendedVideos() {
+  return (
+    <div className='recommendedVideos'>
+      <h2>Here are the RecommendedVideos</h2>
+    </div>
+  )
+}
+
+export default RecommendedVideos
