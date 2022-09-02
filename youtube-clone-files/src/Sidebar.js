a7b5338cc1bcabcc3199eddb50d332986a057792
Juanse1595 <jspbescos15@gmail.com>
2022-09-01 20:33:47 -0500

    ADD SidebarRow.js and .css contains the component which represents every item of the list of the left sidebar, UPD RecommendedVideos.css adding borders to RecommendedVideos section, Sidebar.js adding imports and SidebarRow component

--- youtube-clone-files/src/Sidebar.js
+++ youtube-clone-files/src/Sidebar.js
@@ -1,10 +1,17 @@
-import React from 'react'
-import './Sidebar.css'
+import React from 'react';
+import './Sidebar.css';
+import SidebarRow from './SidebarRow';
+import HomeIcon from '@mui/icons-material/Home';
+import WhatshotIcon from '@mui/icons-material/Whatshot';
+import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
 
 function Sidebar() {
   return (
     <div className='sidebar'>
       <h2>This is the sidebar</h2>
+      <SidebarRow selected title='Home' Icon={HomeIcon} />
+      <SidebarRow title='Trending' Icon={WhatshotIcon} />
+      <SidebarRow title='Subscription' Icon={SubscriptionsIcon} />
     </div>
   )
 }
