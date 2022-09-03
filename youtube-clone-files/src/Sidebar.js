95ae5d71e80c069209838d438fb9ef159c53f7f6
Juanse1595 <jspbescos15@gmail.com>
2022-09-02 19:45:09 -0500

    UPD Sidebar.css with properties of hr, Sidebar.js added the rest of the icons

--- youtube-clone-files/src/Sidebar.js
+++ youtube-clone-files/src/Sidebar.js
@@ -4,6 +4,12 @@ import SidebarRow from './SidebarRow';
 import HomeIcon from '@mui/icons-material/Home';
 import WhatshotIcon from '@mui/icons-material/Whatshot';
 import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
+import VideoLibraryIcon from '@mui/icons-material/VideoLibrary';
+import HistoryIcon from '@mui/icons-material/History';
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
+import WatchLaterIcon from '@mui/icons-material/WatchLater';
+import ThumbsUpAltOutlinedIcon from '@mui/icons-material/ThumbDownAltOutlined';
+import ExpandMoreOutlinedIcon from '@mui/icons-material/ExpandMoreOutlined';
 
 function Sidebar() {
   return (
@@ -12,6 +18,14 @@ function Sidebar() {
       <SidebarRow selected title='Home' Icon={HomeIcon} />
       <SidebarRow title='Trending' Icon={WhatshotIcon} />
       <SidebarRow title='Subscription' Icon={SubscriptionsIcon} />
+      <hr />
+      <SidebarRow title='Library' Icon={VideoLibraryIcon} />
+      <SidebarRow title='History' Icon={HistoryIcon} />
+      <SidebarRow title='Your videos' Icon={OndemandVideoIcon} />
+      <SidebarRow title='Watch Later' Icon={WatchLaterIcon} />
+      <SidebarRow title='Liked videos' Icon={ThumbsUpAltOutlinedIcon} />
+      <SidebarRow title='Show more' Icon={ExpandMoreOutlinedIcon} />
+      <hr />
     </div>
   )
 }
