93daa7a3790de3d7f8eb573dbc5bed1e8a5b404b
Juanse1595 <jspbescos15@gmail.com>
2022-09-08 17:42:10 -0500

    UPD package.json installed react-router-dom App.js started adding routes

--- youtube-clone-files/src/App.js
+++ youtube-clone-files/src/App.js
@@ -2,16 +2,31 @@ import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
 import RecommendedVideos from './RecommendedVideos';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     // BEM class naming convention
     <div className="app">
-      <Header />
-      <div className='app__page'>
-        <Sidebar />
-        <RecommendedVideos />
-      </div>
+      <Router>
+        <Routes>
+          <Route path='/'>
+            <>
+              <Header />
+              <div className='app__page'>
+                <Sidebar />
+                <RecommendedVideos />
+              </div>
+            </>
+          </Route>
+          <Route path='/search'>
+            <>
+            </>
+            <Header />
+            <h1>Search</h1>
+          </Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
