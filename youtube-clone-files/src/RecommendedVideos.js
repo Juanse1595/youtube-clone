d5ae73bcbfa7d489c6a316fe0fc1f32e24889aaa
Juanse1595 <jspbescos15@gmail.com>
2022-09-07 20:16:16 -0500

    ADD VideoCard.js and .css contains the VideoCard components which represents every card with all the elemenets of the video, UPD RecommendedVideos.js and .css added VideoCards

--- youtube-clone-files/src/RecommendedVideos.js
+++ youtube-clone-files/src/RecommendedVideos.js
@@ -1,10 +1,111 @@
 import React from 'react'
 import './RecommendedVideos.css';
+import VideoCard from './VideoCard';
 
 function RecommendedVideos() {
   return (
     <div className='recommendedVideos'>
-      <h2>Here are the RecommendedVideos</h2>
+      <h2>Recommended</h2>
+      <div className='recommendedVideos__videos'>
+        <VideoCard
+          title='Diablo 4 new gameplay'
+          image='https://bit.ly/3ezlbJr'
+          channel='Blizzard' views='1M'
+          timestamp='1 hour ago'
+          channelImage='https://bit.ly/3cSp3EG'
+        />
+        <VideoCard
+          title='Diablo 4 new gameplay'
+          image='https://bit.ly/3ezlbJr'
+          channel='Blizzard' views='1M'
+          timestamp='1 hour ago'
+          channelImage='https://bit.ly/3cSp3EG'
+        />
+        <VideoCard
+          title='Diablo 4 new gameplay'
+          image='https://bit.ly/3ezlbJr'
+          channel='Blizzard' views='1M'
+          timestamp='1 hour ago'
+          channelImage='https://bit.ly/3cSp3EG'
+        />
+        <VideoCard
+          title='Diablo 4 new gameplay'
+          image='https://bit.ly/3ezlbJr'
+          channel='Blizzard' views='1M'
+          timestamp='1 hour ago'
+          channelImage='https://bit.ly/3cSp3EG'
+        />
+        <VideoCard
+          title='Diablo 4 new gameplay'
+          image='https://bit.ly/3ezlbJr'
+          channel='Blizzard' views='1M'
+          timestamp='1 hour ago'
+          channelImage='https://bit.ly/3cSp3EG'
+        />
+        <VideoCard
+          title='Diablo 4 new gameplay'
+          image='https://bit.ly/3ezlbJr'
+          channel='Blizzard' views='1M'
+          timestamp='1 hour ago'
+          channelImage='https://bit.ly/3cSp3EG'
+        />
+        <VideoCard
+          title='Diablo 4 new gameplay'
+          image='https://bit.ly/3ezlbJr'
+          channel='Blizzard' views='1M'
+          timestamp='1 hour ago'
+          channelImage='https://bit.ly/3cSp3EG'
+        />
+        <VideoCard
+          title='Diablo 4 new gameplay'
+          image='https://bit.ly/3ezlbJr'
+          channel='Blizzard' views='1M'
+          timestamp='1 hour ago'
+          channelImage='https://bit.ly/3cSp3EG'
+        />
+        <VideoCard
+          title='Diablo 4 new gameplay'
+          image='https://bit.ly/3ezlbJr'
+          channel='Blizzard' views='1M'
+          timestamp='1 hour ago'
+          channelImage='https://bit.ly/3cSp3EG'
+        />
+        <VideoCard
+          title='Diablo 4 new gameplay'
+          image='https://bit.ly/3ezlbJr'
+          channel='Blizzard' views='1M'
+          timestamp='1 hour ago'
+          channelImage='https://bit.ly/3cSp3EG'
+        />
+        <VideoCard
+          title='Diablo 4 new gameplay'
+          image='https://bit.ly/3ezlbJr'
+          channel='Blizzard' views='1M'
+          timestamp='1 hour ago'
+          channelImage='https://bit.ly/3cSp3EG'
+        />
+        <VideoCard
+          title='Diablo 4 new gameplay'
+          image='https://bit.ly/3ezlbJr'
+          channel='Blizzard' views='1M'
+          timestamp='1 hour ago'
+          channelImage='https://bit.ly/3cSp3EG'
+        />
+        <VideoCard
+          title='Diablo 4 new gameplay'
+          image='https://bit.ly/3ezlbJr'
+          channel='Blizzard' views='1M'
+          timestamp='1 hour ago'
+          channelImage='https://bit.ly/3cSp3EG'
+        />
+        <VideoCard
+          title='Diablo 4 new gameplay'
+          image='https://bit.ly/3ezlbJr'
+          channel='Blizzard' views='1M'
+          timestamp='1 hour ago'
+          channelImage='https://bit.ly/3cSp3EG'
+        />
+      </div>
     </div>
   )
 }
