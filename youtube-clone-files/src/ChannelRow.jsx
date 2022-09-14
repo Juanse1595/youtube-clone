50edc9245848b6f46d1b33691c63a199085202f2
Juanse1595 <jspbescos15@gmail.com>
2022-09-13 21:53:14 -0500

    ADD ChannelRow.jsx and css contains the ChannelRow component, UPD SearchResult.jsx adding ChannelRow component, SearchResult.css adding properties for SearchResult component

---
+++ youtube-clone-files/src/ChannelRow.jsx
@@ -0,0 +1,17 @@
+import { CheckCircleOutlineOutlined } from '@mui/icons-material'
+import { Avatar } from '@mui/material'
+import React from 'react'
+import "./ChannelRow.css"
+
+function ChannelRow({verified, image, channel, subs, noOfVideos, description}) {
+  return (
+    <div className="channelRow">
+      <Avatar className='channelRow__logo' alt={channel} src={image} />
+      <div className="channelRow__text">
+        <h4>{channel} {verified && <CheckCircleOutlineOutlined />}</h4>
+      </div>
+    </div>
+  )
+}
+
+export { ChannelRow }
--- youtube-clone-files/src/SearchResult.jsx
+++ youtube-clone-files/src/SearchResult.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './SearchResult.css';
 // components
+import { ChannelRow } from './ChannelRow';
 import { TuneOutlined } from '@mui/icons-material';
 
 function SearchResult() {
@@ -8,8 +9,17 @@ function SearchResult() {
     <div className='searchResult'>
       <div className="searchResult__filter">
         <TuneOutlined />
-        <p>FILTER</p>
+        <h2>FILTER</h2>
       </div>
+      <hr />
+      <ChannelRow
+        image="https://yt3.ggpht.com/ytc/AMLnZu_GS4vuibuZjr4ZBgVr97RrriUQrrhqeyWQWqMYfQ=s176-c-k-c0x00ffffff-no-rj-mo"
+        channel="Clever Programer"
+        verified
+        subs="1.17M"
+        noOfVideos={450}
+        description="Awesome programers teaching"
+      />
     </div>
   )
 }
